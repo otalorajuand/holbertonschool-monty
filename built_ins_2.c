2e958d743a9812b3f9db803eba800149276da32b
otalorajuand <otalorajuand@gmail.com>
2022-08-31 21:02:35 +0000

    addition of task 2

--- built_ins_2.c
+++ built_ins_2.c
@@ -13,7 +13,7 @@ void func_pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
