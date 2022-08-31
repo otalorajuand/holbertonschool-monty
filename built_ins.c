5b1aa2589e4f4bb0730fea566533e5d6c24b9925
otalorajuand <otalorajuand@gmail.com>
2022-08-31 20:20:46 +0000

    addition of task 1

--- built_ins.c
+++ built_ins.c
@@ -82,7 +82,7 @@ void func_pall(stack_t **stack,
 
 void func_pint(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
