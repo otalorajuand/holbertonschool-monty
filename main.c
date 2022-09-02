f30dcb17f6a29d3d0425e6e2caeafec7cb39f7f6
otalorajuand <otalorajuand@gmail.com>
2022-09-02 16:34:08 +0000

    final version

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 		/* Interpret line */
 		token = strtok(str, " ");
-		if (token == NULL || token[0] == '#' || strcmp(token, "nop"))
+		if (token == NULL || token[0] == '#' || strcmp(token, "nop") == 0)
 		{
 			line_number++;
 			continue;
